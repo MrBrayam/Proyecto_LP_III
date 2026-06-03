0cebcd2e32b63f51c2f314004ffb4a73af31de3d
Dyogo <dyogoazang@gmail.com>
2026-06-02 22:46:57 -0500

    postman excluyed

--- src/main/java/proyecto/lp/iii/api/repository/AlmacenRepository.java
+++ src/main/java/proyecto/lp/iii/api/repository/AlmacenRepository.java
@@ -1,8 +1,9 @@
 package proyecto.lp.iii.api.repository;
 
-import proyecto.lp.iii.api.entity.Almacen;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import proyecto.lp.iii.api.entity.Almacen;
 
 @Repository
 public interface AlmacenRepository extends JpaRepository<Almacen, Integer> {
